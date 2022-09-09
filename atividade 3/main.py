3ea47d3e77e09f635d8d2b492139fc5cdc989434
Vinícius F. Speck <69943624+ViniSpeck@users.noreply.github.com>
2022-09-08 23:03:39 -0300

    Update main.py

--- atividade 3/main.py
+++ atividade 3/main.py
@@ -1,36 +1,40 @@
+from array import array
 from statistics import median
 from threading import Thread
 import pandas as pd
 
-df = pd.read_csv('nomes-brasileiros-ibge/ibge-fem-10000.csv', delimiter=',')
-df2 = pd.read_csv('nomes-brasileiros-ibge/ibge-mas-10000.csv', delimiter=',')
+listaFem = pd.read_csv('./ibge-fem-10000.csv', delimiter=',')
+listaMas = pd.read_csv('./ibge-mas-10000.csv', delimiter=',')
 
-dicionario_fem = df.sort_values("nome").reset_index().loc[:,"nome"].to_dict()
+listaM = listaMas.sort_values("nome").loc[:,"nome"].values.tolist()
+listaF = listaFem.sort_values("nome").loc[:,"nome"].values.tolist()
 
-dicionario_mas = df2.sort_values("nome").reset_index().loc[:,"nome"].to_dict()
+def buscaBinaria(lista, nome):
+    lenght = len(lista)
+    left = 0
+    right = lenght - 1
 
-# classe thread - start - join 
+    while(left <= right):
+        middle = int ((left + right)/2)
+        if lista[middle] == nome:
+            print(nome)
+            return nome
+            break
+        if nome < lista[middle]:
+            right = middle -1
+        if nome > lista[middle]:
+            left = middle + 1
+        else:
+            print("Nome não encontrado")
 
-dicio_inv_fem = dict(map(reversed, dicionario_fem.items()))
+nomeM = input("Insira um nome masculino: ")
+nomeF = input("Insira um nome feminino: ")
 
-dicio_inv_mas = dict(map(reversed, dicionario_mas.items()))
+t1 = Thread(target=buscaBinaria(listaM, nomeM))
+t2 = Thread(target=buscaBinaria(listaF, nomeF))
 
-nome=input("insira um nome (full caps): ")
+t1.start()
+t2.start()
 
-def func(dicio,nome):
-    dicio.get(nome)
-
-if __name__ == '__main__':
-    Thread(target = func).start()
-    Thread(target = func).start()
-
-def verificaHomemMulher(dicio1, dicio2, nome):
-    if __name__ == '__main__':
-        Thread(target = func(dicio1, nome)).start()
-        Thread(target = func(dicio2, nome)).start()
-    if func(dicio1, nome)==func(dicio2, nome):
-        return "é nome de homi e muie"
-    else:
-        return "nao é"
-
-print(func(dicio_inv_fem,nome))
+t1.join()
+t2.join()
